11c825901c09b2ae4603fb4c48f5647955ef5663
zhangchong <731411677@qq.com>
2018-03-12 11:04:00 +0800

    initial commit again

--- src/main/java/com/example/wanandroid/MainActivity.java
+++ src/main/java/com/example/wanandroid/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.wanandroid;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mainDrawerLayout;
+    private RelativeLayout settingRlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainDrawerLayout = (DrawerLayout)findViewById(R.id.dl_main);
+        settingRlayout = (RelativeLayout)findViewById(R.id.rl_setting);
     }
 }
